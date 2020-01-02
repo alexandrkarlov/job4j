f50d6e66dd6d7dd8b6b4ec8b7757ae45cca1e3d1
alexandrkarlov <dr.karlov@gmail.com>
2020-01-02 19:14:19 +0300

    3

---
+++ chapter_0001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMoreAll (){
+        int result = SqMax.max (5, 4, 3, 2);
+        assertThat (result,is (5));
+    }
+    @Test
+    public void whenSecondMoreAll () {
+        int result = SqMax.max (4,5,2,1);
+        assertThat (result,is(5));
+    }
+    @Test
+    public void whenThirdMoreAll () {
+        int result = SqMax.max (2, 3, 4, 1);
+        assertThat (result,is(4));
+    }
+    @Test
+    public void whenFourMoreAll () {
+        int result = SqMax.max (2,1,3,4);
+        assertThat (result, is(4));
+    }
+}
