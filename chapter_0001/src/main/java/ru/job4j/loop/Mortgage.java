dee723df4487f6685ecac9f26c1a0ab17aac3270
alexandrkarlov <dr.karlov@gmail.com>
2020-01-05 23:38:01 +0300

    5.9. Крест в псевдографике. [#214484]

--- chapter_0001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_0001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,13 +3,14 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year (double amount, double salary, double percent){
         int year = 1;
-        double amountRate = (amount * (percent/100));
+        double percentToNumber = percent/100;
+        double amountRate = amount * percentToNumber;
         double ipoteka = (amount + amountRate);
         while (salary < ipoteka){
             amount = ipoteka - salary;
-            amountRate = (amount * (percent/100));
+            amountRate = amount * percentToNumber;
             ipoteka = (amount + amountRate);
-                year = year + 1;
+                year++;
         }
         return year;
     }
