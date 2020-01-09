62a19e203e458372431486b74bcaa011f3bac4ab
alexandrkarlov <dr.karlov@gmail.com>
2020-01-09 10:47:59 +0300

    6.4.1. Слово заканчивается с ...[#214427]

--- chapter_0001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_0001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,10 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
         for(int index = 0; index < pref.length; index++ ) {
-            if(word[index] == pref[index]) {
-                result = true;
-            }
-            else {
-                return result = false;
+            if(word[index] != pref[index]) {
+                result = false;
             }
         }
         return result;
