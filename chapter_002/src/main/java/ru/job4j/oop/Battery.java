08ac3218bf8a48d12c7fedd64a0dcfaa55919860
alexandrkarlov <dr.karlov@gmail.com>
2020-03-28 15:56:44 +0300

    1.7. Состояние объекта.[#214574]

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -11,4 +11,11 @@ public class Battery {
         this.load = this.load + another.load;
         another.load = 0;
     }
+    public static void main(String[] args){
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
 }
